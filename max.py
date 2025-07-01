6e519d38d35b08f73e883127290d7593d0155a6b
shivaboppanapally <shiva3916v@gmail.com>
2025-07-01 10:37:32 +0530

    Add files via upload

--- max.py
+++ max.py
@@ -1,6 +1,12 @@
 a=[1,2,6,7,3,4]
-max=a[0]
+max1=a[0]
 for i in range(len(a)):
-    if(a[i]>max):
-        max=a[i]
-print(max)
+    if(a[i]>max1):
+        max1=a[i]
+print(max1)
+max2=0
+for i in range(len(a)):
+    if a[i]!= max1:
+        if max2 is 0 or a[i]>max2:
+            max2 = a[i]
+print("second max",max2)            
